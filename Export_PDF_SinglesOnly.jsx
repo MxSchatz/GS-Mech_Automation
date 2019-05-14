886ed637eb1957ec55e1fb2fb778d5f97d87ca03
Winter <a.schatz.is@gmail.com>
2019-05-14 11:54:09 -0400

    Update Export_PDF_SinglesOnly.jsx
    Migrated cover exports to the SinglesOnly script.

--- Export_PDF_SinglesOnly.jsx
+++ Export_PDF_SinglesOnly.jsx
@@ -20,6 +20,8 @@ var myFileName;
 var name1;
 var name2;
 var name3;
+var name4;
+var name5;
 var preset1 = app.pdfExportPresets.itemByName("LR_MOM_optimize"); 
 var preset2 = app.pdfExportPresets.itemByName("HR Layers"); 
 var preset3 = app.pdfExportPresets.itemByName("HRcropsbleed Layers");
@@ -39,9 +41,13 @@ function main(){
     if (doc.modified == false){  
 
         // if saved, run the export functions
+        
         LR_MOM_singles();
         HR_singles();
         HR_crops();
+        LR_MOM_coverPDF();
+        LR_MOM_coverJPG();
+        
         myTeardown();  
 
     } else {   
@@ -111,13 +117,64 @@ function HR_crops(){
 	var preset3 = app.pdfExportPresets.itemByName("HRcropsbleed Layers");
 	app.pdfExportPresets.itemByName("HRcropsbleed Layers");
 	
-	name3 = thePath+"_HRcrops.pdf";
-	// d.exportFile(ExportFormat.PDF_TYPE, new File(name3), false, preset3); 
+	name3 = thePath+"_HRcrops.pdf"; 
 
 }
 
 // file suffixes
  
+function LR_MOM_coverJPG(){  
+
+    //Sets PDF export options, then exports the active document as PDF. 
+    var myDocument = app.activeDocument;   
+    var myFileName;
+    var page = app.activeWindow.activePage;
+      
+    with(app.jpegExportPreferences){ 
+
+    // app.pdfExportPresets.itemByName("LR_MOM_optimize");
+    jpegExportRange = ExportRangeOrAllPages.EXPORT_RANGE;
+    pageRange = myDocument.layoutWindows[0].activePage.name;
+    exportingSpread = false;
+    pageString = page.name;
+    exportResolution = 72; // The export resolution expressed as a real number instead of an integer. (Range: 1.0 to 2400.0)
+    antiAlias = true; //  If true, use anti-aliasing for text and vectors during export
+    embedColorProfile = false; // True to embed the color profile, false otherwise
+    jpegColorSpace = JpegColorSpaceEnum.RGB; // One of RGB, CMYK or GRAY
+    jpegQuality = JPEGOptionsQuality.LOW; // The compression quality: LOW / MEDIUM / HIGH / MAXIMUM
+    jpegRenderingStyle = JPEGOptionsFormat.BASELINE_ENCODING; // The rendering style: BASELINE_ENCODING or PROGRESSIVE_ENCODING
+    simulateOverprint = false; // If true, simulates the effects of overprinting spot and process colors in the same way they would occur when printing
+    useDocumentBleeds = false; // If true, uses the document's bleed settings in the exported JPEG.
+    }  
+    //Now export the document. You'll have to fill in your own file path.
+    
+    myFileName = myDocument.fullName + ""; 
+ 
+    var myRegularExpression = /.indd/gi;   
+    // myFileName = myFileName.replace(myRegularExpression, "_LRcover.jpg");
+    name4 = thePath+"_LRcover.jpg";
+    //asynchronousExportFile doesn't work for JPG format, this will be done before 
+    // myDocument.ExportFile(ExportFormat.JPG, new File(myFileName), false);
+}
+
+
+function LR_MOM_coverPDF(){   
+
+    //Sets PDF export options, then exports the active document as PDF.      
+    with(app.pdfExportPreferences){  
+        pageRange = myDocument.layoutWindows[0].activePage.name;
+        exportingSpread = false;
+        pdfExportRange = ExportRangeOrAllPages.EXPORT_RANGE;
+        pageString = page.name;
+        useDocumentBleeds = false; // If true, uses the document's bleed settings 
+    }  
+    // custom export profile, which overrides anything above
+    app.pdfExportPresets.itemByName("LR_MOM_optimize");
+
+    //Now export the document. You'll have to fill in your own file path.      	
+	name4 = thePath+"_LRcover.pdf";
+}
+
 
 /* WIP Single pages
 cover1 = thePath+"LRcover.pdf";
@@ -125,16 +182,17 @@ cover2 = thePath+"LRcover.jpg";
 */
 
 
-//export location - covers
-/* WIP
-p.exportFile(ExportFormat.PDF_TYPE, new File(cover1), false, preset1);
-i.exportFile(ExportFormat.JPG, new File(cover2), false, preset1);
-*/
 
 function myTeardown(){  
-
+	/*
+	exportFile(ExportFormat.PDF_TYPE, new File(cover1), false, preset1);
+	*/
 	d.asynchronousExportFile(ExportFormat.PDF_TYPE, new File(name1), false, preset1);
 	d.asynchronousExportFile(ExportFormat.PDF_TYPE, new File(name2), false, preset2);
 	d.asynchronousExportFile(ExportFormat.PDF_TYPE, new File(name3), false, preset3);
+	//covers / singles
+	d.asynchronousExportFile(ExportFormat.PDF_TYPE, new File(name4), false, preset1);
+	myDocument.ExportFile(ExportFormat.JPG, new File(name5), false);
+
     alert("Mech exports have started! Check background tasks for progress.");
 }  
